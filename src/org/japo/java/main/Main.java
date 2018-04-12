e52e92144da7e1347b9a58301fad9e6f193c9733
halloween <halloween@gmail.com>
2018-04-12 08:47:51 +0200

    :joy: actu version

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -25,21 +25,21 @@ import org.japo.java.libraries.UtilesApp;
  * @author José A. Pacheco Ondoño - joanpaon@gmail.com
  */
 public class Main {
+
     // Fichero Propiedades App
     public static final String FICHERO_PROPIEDADES = "app.properties";
 
     // Entrada a la Aplicación
     public static void main(String[] args) {
-        // Lanzar GUI
-        
-            // Cargar Propiedades App
-            Properties prp = UtilesApp.importarPropiedades(FICHERO_PROPIEDADES);
-            
-            // Instanciar GUI
-            GUI gui = new GUI(prp);
 
-            // Mostrar GUI
-            gui.setVisible(true);
-        
+        // Cargar Propiedades App
+        Properties prp = UtilesApp.importarPropiedades(FICHERO_PROPIEDADES);
+
+        // Instanciar GUI
+        GUI gui = new GUI(prp);
+
+        // Mostrar GUI
+        gui.setVisible(true);
+
     }
 }
